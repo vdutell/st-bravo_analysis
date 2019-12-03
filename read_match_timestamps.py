7fb1cfe2e3b6432ef418447b57695a38fba5329f
vdutell <vashadutell@gmail.com>
2019-12-03 02:21:02 +0000

    use pupil timestamps from world cam overlay when matching timestamps

--- read_match_timestamps.py
+++ read_match_timestamps.py
@@ -1,6 +1,7 @@
 import os, stat
 import numpy as np
 import utils.run_analysis as ana
+import datatable as dt
 
 def read_match_timestamps(base_dir, target_fps, subject, trial, num_cameras=1):
     '''
@@ -37,7 +38,7 @@ def read_match_timestamps(base_dir, target_fps, subject, trial, num_cameras=1):
     ximea_timesync_file = os.path.join(trial_directory, 'scene_camera', f'timestamp_camsync_{cam}.tsv')
 
     #pupil (eye cameras)
-    pupil_timestamp_file = os.path.join(trial_directory, 'eye_camera','000', 'pupil_timestamps.npy')
+    pupil_timestamp_file = os.path.join(trial_directory, 'eye_camera','000','exports','000','pupil_positions.csv')
     pupil_annotations_file = os.path.join(trial_directory, 'eye_camera','000', 'annotation_timestamps.npy')
 
     analysis_folder = os.path.join(output_dir, subject, trial,'')
@@ -52,7 +53,13 @@ def read_match_timestamps(base_dir, target_fps, subject, trial, num_cameras=1):
     
     pupil_num='000'
 
-    pupil_ts = np.load(pupil_timestamp_file)[::2]
+    #get pupil timestamps
+    pupil_positions = dt.fread(pupil_timestamp_file)
+    pupil_positions = pupil_positions[:,[0,1,2,4,5]]
+    pupil_eye_0 = np.array(pupil_positions)[np.where(np.array(pupil_positions)[:,2]==1)[0],:]
+    pupil_eye_1 = np.array(pupil_positions)[np.where(np.array(pupil_positions)[:,2]==0)[0],:]
+    pupil_ts_eye0 = pupil_eye_0[:,0]
+    pupil_ts_eye1 = pupil_eye_1[:,0]
     pupil_annotations = np.load(pupil_annotations_file)
 
     #ximea timestamps
@@ -71,23 +78,27 @@ def read_match_timestamps(base_dir, target_fps, subject, trial, num_cameras=1):
     print(f'Task Lasted: {end_task_time-start_task_time} seconds.')
   
     
-    start_timestamp = np.max((timestamps_ximea[0], pupil_ts[0]))
-    end_timestamp = np.min((timestamps_ximea[-1], pupil_ts[-1]))
+    start_timestamp = np.max((timestamps_ximea[0], pupil_ts_eye0[0], pupil_ts_eye1[0]))
+    end_timestamp = np.min((timestamps_ximea[-1], pupil_ts_eye0[-1],pupil_ts_eye1[-1]))
     common_timeline = np.arange(start_timestamp, end_timestamp, 1./target_fps)
 
     ximea_common_timeline_match = np.zeros_like(common_timeline)
-    pupil_common_timeline_match = np.zeros_like(common_timeline)
+    pupil_eye0_common_timeline_match = np.zeros_like(common_timeline)
+    pupil_eye1_common_timeline_match = np.zeros_like(common_timeline)
     during_task = np.zeros_like(common_timeline)
     during_calibration = np.zeros_like(common_timeline)
 
     for i, t in enumerate(common_timeline):
         ximea_common_timeline_match[i] = np.argmin(np.abs(timestamps_ximea - t))
-        pupil_common_timeline_match[i] = np.argmin(np.abs(pupil_ts - t))
+        pupil_eye0_common_timeline_match[i] = np.argmin(np.abs(pupil_ts_eye0 - t))
+        pupil_eye1_common_timeline_match[i] = np.argmin(np.abs(pupil_ts_eye1 - t))
         if((t > start_task_time) and (t < end_task_time)):
             during_task[i] = 1
             
-    common_timeline_table = np.array((common_timeline, ximea_common_timeline_match, pupil_common_timeline_match, during_task, during_calibration)).T
-    common_timeline_table_colnames = 'common_timeline\tximea_frame\tpupil_frame\tduring_task\tduring_calibration'
+            
+    common_timeline_table = np.array((common_timeline, ximea_common_timeline_match, pupil_eye0_common_timeline_match, pupil_eye1_common_timeline_match, during_task, during_calibration)).T
+    common_timeline_table_colnames = 'common_timeline\tximea_frame\tpupil_eye0_frame\tpupil_eye1_frame\tduring_task\tduring_calibration'
+    common_timeline_file = os.path.join(analysis_folder,'common_timeline.tsv')            
     common_timeline_file_human = os.path.join(analysis_folder,'common_timeline.tsv')
     np.savetxt(common_timeline_file_human, common_timeline_table, delimiter='\t', header=common_timeline_table_colnames)
     common_timeline_file = os.path.join(analysis_folder,'common_timeline.npy')
